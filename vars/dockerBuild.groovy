a180168adb6bbd81e01e249d78d28d6f83ec3066
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-28 23:14:42 +0200

    Updated dockerBuild script

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,7 +10,7 @@ def call(config) {
     }
 
     sh """
-        docker build -f $(basename ${filename}) $(dirname ${filename})
+        docker build -f \$(basename ${filename}) $(dirname ${filename})
     """
 }
 
