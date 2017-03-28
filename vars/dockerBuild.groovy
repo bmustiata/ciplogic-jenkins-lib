0aecb99442432f00df93d6873ad48bb79b6daaf9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-28 23:09:51 +0200

    Updated dockerBuild script

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,10 @@
+/**
+ * Build a docker container. This just allows extra setting build arguments,
+ * and a bunch of tags on the end container.
+ */
+def call(config) {
+  sh """
+    ls -la
+  """
+}
+
