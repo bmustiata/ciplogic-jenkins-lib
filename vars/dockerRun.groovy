5059f21af03afef36ce7edc57d77e1945da04637
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-03-29 23:06:17 +0200

    dockerRun changes

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,3 +1,43 @@
 def call(config) {
+    def image = config?.image
+    def command = config?.command ?: ""
+
+    def env = config?.env ?: []
+    def links = config?.links ?: []
+    def ports = config?.ports ?: []
+    def volumes = config?.volumes ?: []
+
+    if (!image) {
+        print "You need to specify what image to run, with image:..."
+        throw new IllegalArgumentException("Missing `image` name")
+    }
+
+    def stringEnv = ''
+    for (int i = 0; i < env.size(); i++) {
+        stringEnv += "-e '" + env.get(i) + "' "
+    }
+
+    def stringLinks = ''
+    for (int i = 0; i < links.size(); i++) {
+        stringLinks += "--link '" + links.get(i) + "' "
+    }
+
+    def stringPorts = ''
+    for (int i = 0; i < ports.size(); i++) {
+        stringPorts += "-p '" + ports.get(i) + "' "
+    }
+
+    def stringVolumes = ''
+    for (int i = 0; i < volumes.size(); i++) {
+        stringVolumes += "-v '" + volumes.get(i) + "' "
+    }
+
+    def script = """
+        docker run ${stringEnv}${stringLinks}${stringPorts}${stringVolumes}${image} ${command}
+    """
+
+    print "Going to run:\n$script"
+
+    sh script
 }
 
