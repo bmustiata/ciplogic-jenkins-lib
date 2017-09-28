582fa12ac61053b92464ebcf5ffbd12ae3d5c5d8
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-09-29 00:23:51 +0200

    Read first the port.

--- vars/getRandomPort.groovy
+++ vars/getRandomPort.groovy
@@ -4,17 +4,13 @@
 def call() {
   def port = -1
 
-  for (int i = 0; i < 5; i++) {
-    try {
-      new ServerSocket(0).withCloseable({s ->
-          s.setReuseAddress(true)
-          port = s.getLocalPort()
-      })
-
-      return port
-    } catch (Exception e) {
-      // ignore on purpose
-    }
+  try {
+    new ServerSocket(0).withCloseable({s ->
+        port = s.getLocalPort()
+        s.setReuseAddress(true)
+    })
+  } catch (Exception e) {
+    // ignore on purpose
   }
 
   port
