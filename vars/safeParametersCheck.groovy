86e34b8db55ff1851b80ad8769a88fe1d7d9e846
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-06-12 22:53:07 +0200

    symbol

--- vars/safeParametersCheck.groovy
+++ vars/safeParametersCheck.groovy
@@ -4,6 +4,16 @@ def call(binding) {
     safeParameters.each {
         if (!binding.hasVariable(it.arguments.name)) {
             binding.setVariable(it.arguments.name, it.arguments.defaultValue)
+            return
+        }
+
+        // convert the booleans correctly
+        def value = binding.getVariable(it.arguments.name)
+
+        print("symbol: ${it.symbol}")
+
+        if (it.symbol == 'boolean' && !(value instanceof Boolean)) {
+            binding.setVariable(it.arguments.name, Boolean.valueOf(value))
         }
     }
 }
