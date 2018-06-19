e0fccbb569da4979246b3d25bcd4b607a562b32d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-06-19 20:38:23 +0200

    Make sure the archiving happens even if an exception is thrown in the execution.

--- vars/junitReports.groovy
+++ vars/junitReports.groovy
@@ -2,16 +2,21 @@
  * Makes sure that the reports are in the correct folder for processing them
  * with junit. This will take care that the folder is in the workspace, and
  * will clean it up after.
+ *
+ * This must be called from the workspace.
  */
 def call(reportsFolder, code) {
     def reportsLocation = getGuid("reports-")
+    def currentFolder = pwd()
 
     try {
+        echo "JUnit code block."
         code()
 
-        sh "mv '${reportsFolder}' '${reportsLocation}'"
     } finally {
-        junit "${reportsLocation}/*.xml"
+        echo "Running JUnit archival"
+        sh "mv '${reportsFolder}' '${currentFolder}/${reportsLocation}'"
+        junit "${currentFolder}/${reportsLocation}/*.xml"
         sh "rm -fr ./${reportsLocation}"
     }
 }
