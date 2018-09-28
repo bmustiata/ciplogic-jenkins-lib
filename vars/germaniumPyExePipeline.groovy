876290f79dfe12eb28a365fd52ec30417b0c173f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-09-28 06:44:43 +0200

    Added optional checks.

--- vars/germaniumPyExePipeline.groovy
+++ vars/germaniumPyExePipeline.groovy
@@ -1,17 +1,38 @@
 def call(config) {
+    properties([
+        safeParameters(config.that, [
+            booleanParam(name: 'RUN_MYPY_CHECKS', defaultValue: true,
+                    description: 'Run the mypy type checks.'),
+            booleanParam(name: 'RUN_FLAKE8_CHECKS', defaultValue: true,
+                    description: 'Run the flake8 linting.')
+        ])
+    ])
+
+    safeParametersCheck(config.that)
+
     stage('Type Check') {
+        if (!RUN_MYPY_CHECKS && !RUN_FLAKE8_CHECKS) {
+            return;
+        }
+
         node {
             deleteDir()
             checkout scm
 
             docker.build("mypy_${env.BUILD_ID}",
                          '-f Dockerfile.py.build .')
-                  .inside("-v ${pwd()}:/src")
-            {
-                sh """
-                    cd /src
-                    mypy application.py
-                """
+                  .inside("-v ${pwd()}:/src") {
+                if (RUN_MYPY_CHECKS) {
+                    sh """
+                        mypy .
+                    """
+                }
+
+                if (RUN_FLAKE8_CHECKS) {
+                    sh """
+                        flake8 .
+                    """
+                }
             }
         }
     }
