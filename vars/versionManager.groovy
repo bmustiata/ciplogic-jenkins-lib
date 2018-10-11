061a8f70c3a850cd208a1f30b1c7888f63ab5561
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-11 05:33:51 +0200

    Use suffix for parallel executions.

--- vars/versionManager.groovy
+++ vars/versionManager.groovy
@@ -7,14 +7,14 @@ def call(extraCommand) {
 
     try {
         sh """
-            mkdir -p /tmp/version-manager/
-            docker cp ${VERSION_MANAGER_IMAGE_NAME}:/src/dist/version-manager /tmp/version-manager/version-manager
-            chmod +x /tmp/version-manager/version-manager
-            PATH="/tmp/version-manager:\$PATH"
+            mkdir -p /tmp/version-manager_${suffix}/
+            docker cp ${VERSION_MANAGER_IMAGE_NAME}:/src/dist/version-manager /tmp/version-manager_${suffix}/version-manager
+            chmod +x /tmp/version-manager_${suffix}/version-manager
+            PATH="/tmp/version-manager_${suffix}:\$PATH"
             version-manager ${extraCommand}
         """
     } finally {
-        sh "rm -fr /tmp/version-manager || true"
+        sh "rm -fr /tmp/version-manager_${suffix} || true"
     }
 }
 
