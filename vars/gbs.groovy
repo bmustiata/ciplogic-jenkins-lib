cb760e0a71c6db2e8d36b3b0b26e7bf9220024a4
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-25 05:49:41 +0200

    gbs docker layers

---
+++ vars/gbs.groovy
@@ -0,0 +1,137 @@
+def createDockerImage(config, dockerfileContent) {
+    def dockerFileName = "/tmp/_Dockerfile.gbs-${getGuid()}"
+    def dockerFile = new File(dockerFileName)
+
+    try {
+        dockerFile.write dockerfileContent.stripIndent()
+
+        return docker.build(config.dockerTag, "-f ${dockerFileName} .")
+    } finally {
+        dockerFile.delete()
+    }
+}
+
+def call() {
+    return [
+        test: { config ->
+            def GBS_PREFIX = config.prefix ?: '/'
+            return createDockerImage(config,
+                """\
+                FROM germaniumhq/${config.platform}
+
+                #======================================
+                # Install prerequisite software
+                #======================================
+                USER root
+
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/install-software /src${GBS_PREFIX}_gbs/install-software
+                RUN echo "################################################################################" &&\
+                    echo "# INSTALL SOFTWARE" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/install-software/install-software.sh &&\
+                    chown -R germanium:germanium /src
+
+                #======================================
+                # Prepare dependencies for download
+                #======================================
+                USER germanium
+
+                # build1
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build1 /src${GBS_PREFIX}_gbs/prepare-build1
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 1" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build1/prepare-build1.sh
+
+                # build2
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build2 /src${GBS_PREFIX}_gbs/prepare-build2
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 2" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build2/prepare-build2.sh
+
+                # build3
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build3 /src${GBS_PREFIX}_gbs/prepare-build3
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 3" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build3/prepare-build3.sh
+
+                # test1
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-test1 /src${GBS_PREFIX}_gbs/prepare-test1
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE TEST 1" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-test1/prepare-test1.sh
+
+                # test2
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-test2 /src${GBS_PREFIX}_gbs/prepare-test2
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE TEST 2" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-test2/prepare-test2.sh
+
+                # sources are copied only after the test stage
+                COPY --chown=germanium:germanium . /src
+                """
+            )
+        },
+
+        build: { config ->
+            def GBS_PREFIX = config.prefix ?: '/'
+            return createDockerImage(config,
+                """\
+                FROM germaniumhq/${config.platform}
+
+                #======================================
+                # Install prerequisite software
+                #======================================
+                USER root
+
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/install-software /src${GBS_PREFIX}_gbs/install-software
+                RUN echo "################################################################################" &&\
+                    echo "# INSTALL SOFTWARE" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/install-software/install-software.sh &&\
+                    chown -R germanium:germanium /src
+
+                #======================================
+                # Prepare dependencies for download
+                #======================================
+                USER germanium
+
+                # build1
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build1 /src${GBS_PREFIX}_gbs/prepare-build1
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 1" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build1/prepare-build1.sh
+
+                # build2
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build2 /src${GBS_PREFIX}_gbs/prepare-build2
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 2" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build2/prepare-build2.sh
+
+                # build3
+                COPY --chown=germanium:germanium ${GBS_PREFIX}_gbs/prepare-build3 /src${GBS_PREFIX}_gbs/prepare-build3
+                RUN echo "################################################################################" &&\
+                    echo "# PREPARE BUILD 3" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/prepare-build3/prepare-build3.sh
+
+                # sources are copied only after the test stage
+                COPY --chown=germanium:germanium . /src
+
+                # run the build
+                RUN echo "################################################################################" &&\
+                    echo "# RUN BUILD" && \
+                    echo "################################################################################" &&\
+                    /src${GBS_PREFIX}_gbs/run-build.sh
+                """
+            )
+        }
+
+    ]
+}
